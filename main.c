79a08c682ba00567335072a3c0fd8b5eef0e8f39
robsy121 <robert.hofmann95@gmail.com>
2018-12-08 16:59:24 +0100

    add method for reading response

--- main.c
+++ main.c
@@ -8,12 +8,14 @@
 #include <unistd.h>
 
 void usage(FILE *stream, const char *cmnd, int exitcode);
+
 int connectToServer(const char *server, const char *port);
+
 int send_req(FILE *write_fd, int sfd, const char *user, const char *message, const char *img_url);
+
 int read_resp(FILE *read_fd);
 
-int main(const int argc, const char * const argv[])
-{
+int main(const int argc, const char *const argv[]) {
 
     const char *server;
     const char *port;
@@ -25,7 +27,7 @@ int main(const int argc, const char * const argv[])
     FILE *write_fd = NULL;
     FILE *read_fd = NULL;
 
-    smc_parsecommandline(argc, argv, usage, &server, &port, &user, &message,&img_url, &verbose);
+    smc_parsecommandline(argc, argv, usage, &server, &port, &user, &message, &img_url, &verbose);
 
     printf("Server:  %s\n", server);
     printf("Port:    %s\n", port);
@@ -37,17 +39,17 @@ int main(const int argc, const char * const argv[])
     sfd = connectToServer(server, port);
 
     write_fd = fdopen(sfd, "w");
-    if (write_fd == NULL){
+    if (write_fd == NULL) {
         fprintf(stderr, "Could not open write fd\n");
         fclose(write_fd);
         exit(EXIT_FAILURE);
     }
 
-    send_req(write_fd,sfd,user,message,img_url);
-    shutdown(sfd,SHUT_WR);
+    send_req(write_fd, sfd, user, message, img_url);
+    shutdown(sfd, SHUT_WR);
 
     read_fd = fdopen(sfd, "r");
-    if (read_fd == NULL){
+    if (read_fd == NULL) {
         fprintf(stderr, "Could not open read fd\n");
         fclose(write_fd);
         exit(EXIT_FAILURE);
@@ -64,8 +66,7 @@ int main(const int argc, const char * const argv[])
     return 0;
 }
 
-int connectToServer(const char *server, const char *port)
-{
+int connectToServer(const char *server, const char *port) {
 
     int sfd, s;
     struct addrinfo hints;
@@ -76,14 +77,12 @@ int connectToServer(const char *server, const char *port)
     hints.ai_socktype = SOCK_STREAM; /* Stream socket */
 
     s = getaddrinfo(server, port, &hints, &result);
-    if (s != 0)
-    {
+    if (s != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
         exit(EXIT_FAILURE);
     }
 
-    for (rp = result; rp != NULL; rp = rp->ai_next)
-    {
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
         sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
         if (sfd == -1)
             continue;
@@ -92,8 +91,7 @@ int connectToServer(const char *server, const char *port)
 
     }
 
-    if (rp == NULL)
-    { /* No address succeeded */
+    if (rp == NULL) { /* No address succeeded */
         fprintf(stderr, "Could not connect\n");
         exit(EXIT_FAILURE);
     }
@@ -102,28 +100,7 @@ int connectToServer(const char *server, const char *port)
     return sfd;
 }
 
-//https://cis.technikum-wien.at/documents/bic/3/vcs/download/vcs_tcpip/bgnet_A4.pdf Page 38
-int sendall(int s, char *buf, int *len)
-{
-    int total = 0; // how many bytes we've sent
-    int bytesleft = *len; // how many we have left to send
-    int n;
-    while (total < *len)
-    {
-        n = send(s, buf + total, bytesleft, 0);
-        if (n == -1)
-        {
-            break;
-        }
-        total += n;
-        bytesleft -= n;
-    }
-    *len = total; // return number actually sent here
-    return n == -1 ? -1 : 0; // return -1 on failure, 0 on success
-}
-
-void usage(FILE *stream, const char *cmnd, int exitcode)
-{
+void usage(FILE *stream, const char *cmnd, int exitcode) {
     fprintf(stream, "usage: %s options\n", cmnd);
     fprintf(stream, "options:\n");
     fprintf(stream, "        -s, --server <server>   full qualified domain name or IP address of the server\n");
@@ -136,7 +113,7 @@ void usage(FILE *stream, const char *cmnd, int exitcode)
     exit(exitcode);
 }
 
-int send_req(FILE *write_fd, int sfd, const char *user, const char *message, const char *img_url){
+int send_req(FILE *write_fd, int sfd, const char *user, const char *message, const char *img_url) {
 
     const char *pre_user = "user=";
     const char *pre_message = "\n";
@@ -145,9 +122,9 @@ int send_req(FILE *write_fd, int sfd, const char *user, const char *message, con
 
     if (img_url != NULL) {
         pre_img_url = "\nimg=";
-    }else{
-        pre_img_url="";
-        img_url="";
+    } else {
+        pre_img_url = "";
+        img_url = "";
     }
 
     //size_t msg_length = strlen(pre_user) + strlen(user) + strlen(pre_img_url) + strlen(img_url) + strlen(pre_message) + strlen(message) + 1;
@@ -156,7 +133,7 @@ int send_req(FILE *write_fd, int sfd, const char *user, const char *message, con
     //fprintf(write_fd, "%s",formatedString);
     //free(formatedString);
 
-    fprintf(write_fd, "%s%s%s%s%s%s",pre_user,user,pre_img_url,img_url,pre_message,message);
+    fprintf(write_fd, "%s%s%s%s%s%s", pre_user, user, pre_img_url, img_url, pre_message, message);
 
     fflush(write_fd);
 
@@ -164,28 +141,81 @@ int send_req(FILE *write_fd, int sfd, const char *user, const char *message, con
 
 }
 
-int read_resp(FILE *read_fd){
-    //parsing stage = GET_STATUS;
+int read_resp(FILE *read_fd) {
+    const int buffersize = 200;
     char *line = NULL;
-    //char file_name[NAME_MAX];
+    char buffer[buffersize];
+    char *file_name = NULL;
     long status = -1;
     long file_len = 0;
     long counter = 0;
-    ssize_t read = -1;
+    long toprocess = 0;
     size_t len = 0;
     FILE *fp = NULL;
-    char *value;
 
-    if ((getline(&line,&len,read_fd)) != -1){
-        printf("line: %s",line);
+    if ((getline(&line, &len, read_fd)) != -1) {
+        printf("line: %s", line);
         strtok(line, "=");
-        value = strtok(NULL, "\n");
+        status = strtol(strtok(NULL, "\n"), NULL, 10);
 
-        printf("value: %s",value);
+        printf("status: %ld", status);
+    } else {
+        printf("Cannot read Status");
+    }
+
+    while ((getline(&line, &len, read_fd)) != -1) {
+        //get file
+        printf("line: %s", line);
+        strtok(line, "=");
+        file_name = strtok(NULL, "\n");
+
+        printf("file_name: %s", file_name);
+
+        //create/open file
+        if ((fp = fopen(file_name, "w+")) == NULL) {
+            break;
+        }
+
+        //get len
+        if ((getline(&line, &len, read_fd)) != -1) {
+            printf("line: %s", line);
+            strtok(line, "=");
+            file_len = strtol(strtok(NULL, "\n"), NULL, 10);
+
+            printf("len: %ld", file_len);
+        } else {
+            printf("Cannot read len");
+        }
+
+        counter = file_len;
+
+        while (counter != 0) {
+            //get data
+            // memset(buffer, '\0', buffersize);
+            toprocess = counter;
+            if (toprocess > buffersize){
+                toprocess = buffersize;
+            }
+
+            fread(buffer, 1, (size_t)toprocess, read_fd);
+
+            printf("\n\n%ld - %zd\n\n",toprocess,(size_t)toprocess);
+            counter -= toprocess;
+
+            printf("%s", buffer);
+
+            fwrite(buffer, sizeof(char), (size_t)toprocess, fp);
+
+
+            }
+
+        if (fclose(fp) == EOF) {
+            break;
+
+        }
 
     }
 
+    return 0;
 
 }
-
-int parse_status
